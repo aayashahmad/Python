ae85a7b6dbb539b37c4a7521f6da1870f2f59adc
aayashahmad <bhatashu666@gmail.com>
2023-11-23 19:03:33 +0530

    PROGS

---
+++ multiplecation.py
@@ -0,0 +1,4 @@
+num1 = 2
+num2 = 2
+result = num1 * num2
+print('The result of {0} and {1} is {2}'.format(num1, num2, result))
---
+++ table.py
@@ -0,0 +1,7 @@
+num = 3
+
+for i in range(100):
+    # print(i)
+    if i == 11 : break
+    result = num * i
+    print('{0} * {1} = {2}'.format(num, i, result))
