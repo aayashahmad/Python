4fbf1efd4d1e7a954d414be6e6a78a627906efb1
aayashahmad <bhatashu666@gmail.com>
2023-11-27 18:16:38 +0530

    add 2 prog

---
+++ findgreatestnum.py
@@ -0,0 +1,10 @@
+x=1
+y=2
+z=3
+print(max(x,y,z))
+
+print("ex 2")
+
+lis = [100,40,400,600]
+print("largest number in the list is:",
+      max(lis))
---
+++ greatest2num.py
@@ -0,0 +1,9 @@
+# num, num2 = 20, 30
+# if num>num2:
+#     print(num)
+# else:
+#     print(num2)
+
+# using max() func
+num,num2=20,30
+print(max(num,num2))
